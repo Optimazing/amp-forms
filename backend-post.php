8b931d05441990e91ed3cbdf42ae6c0e9afac330
Heriberto A Cortes Bravo <hcortesb@gmail.com>
2018-07-05 12:07:16 +0200

    Quitar OPTIONS en header allow methods

--- backend-post.php
+++ backend-post.php
@@ -2,7 +2,7 @@
 if (!empty($_POST)) {
     header("access-control-allow-credentials:true");
     header("access-control-allow-headers:Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token");
-    header("access-control-allow-methods:POST, GET, OPTIONS");
+    header("access-control-allow-methods:POST, GET");
     header("access-control-allow-origin:".$_SERVER['HTTP_ORIGIN']);
     header("access-control-expose-headers:AMP-Access-Control-Allow-Source-Origin");
     // change to represent your site's protocol, either http or https
